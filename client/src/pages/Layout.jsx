97673d67715e491c4e1343379a31a3000b058e6d
lucianochimale8 <kchimale@gmail.com>
2025-11-02 19:49:22 -0300

    Agregando componentes al layout para mas personalizacion, esta en proceso el diseño

--- client/src/pages/Layout.jsx
+++ client/src/pages/Layout.jsx
@@ -1,25 +1,14 @@
-import { Outlet, Link } from "react-router-dom";
+import { Outlet } from "react-router-dom";
+import Navbar from "../common/Navbar";
+import Header from "../common/Header";
+
 
 export default function Layout(){
   
     return (
         <>
-            <h1>Layout</h1>
-            <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-                <div className="container-fluid">
-                    <Link className="navbar-brand" to="/">FPW</Link>
-                    <div className="collapse navbar-collapse" id="navbarNav">
-                        <ul className="navbar-nav">
-                            <li className="nav-item">
-                                <Link className="nav-link" to="/home">Home</Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link className="nav-link" to="/proyects">Proyectos</Link>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </nav>
+            <Header/>
+            <Navbar/>
             <Outlet/>
         </>
     )
