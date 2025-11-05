a2e320030ab05ee4e0bd5cde302062d86fd1888d
Victor Caro <sbigtheo@gmail.com>
2025-11-05 19:59:39 -0300

    Caro Victor: navbar para las actividades del proyecto 5 ajustadas

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,8 @@ import Proyecto2 from "./components/Proyecto2/Proyecto2";
 import Proyecto3 from "./components/Proyecto3/Proyecto3";
 import AdivinaElNumero from "./components/Proyecto4/AdivinaElNumero/GuessTheNumber";
 import JuegoDeColores from "./components/Proyecto4/JuegoDeColores/GameOfColours";
-import Proyecto5 from "./components/Proyecto5/Proyecto5";
+import Formulario from "./components/Proyecto5/FormularioDeRegistro/Formulario";
+import JuegoEstrellas from "./components/Proyecto5/JuegoEstrellas/Estrellas";
 import AboutMiembros from "./common/AboutMiembros";
 
 function App() {
@@ -19,7 +20,8 @@ function App() {
         <Route path="/proyecto3" element={<Proyecto3 />} />
         <Route path="/proyecto4/AdivinaElNumero" element={<AdivinaElNumero />} />
         <Route path="/proyecto4/JuegoDeColores" element={<JuegoDeColores />} />
-        <Route path="/proyecto5" element={<Proyecto5 />} />
+        <Route path="/proyecto5/FormularioDeRegistro" element={<Formulario />} />
+        <Route path="/proyecto5/JuegoEstrellas" element={<JuegoEstrellas />} />
         <Route path="/AboutMiembros" element={<AboutMiembros />} />
       </Routes>
     </Router>
--- client/src/components/Proyecto5/Proyecto5.jsx
+++ client/src/components/Proyecto5/Proyecto5.jsx
@@ -1,13 +1,10 @@
-import React from "react";
-
-import Formulario from "./Formulario";
+import Formulario from "./FormularioDeRegistro/Formulario";
 import Estrellas from "./Estrellas";
 
 export default function Proyecto5() {
   return (
     <div style={{ margin: "20px" }}>
       <h1>Proyecto 5</h1>
-      {/*<AboutMiembros />*/}
       <Formulario />
       <Estrellas />
     </div>
